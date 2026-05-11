d91b84526f0674cabaeaf6d786dcea88a31f0e78
Pedro Enrique Traverso García <63427425+petravgar@users.noreply.github.com>
2026-05-11 17:37:27 +0200

    Commit 1: Configuración inicial de Express
    Ya hemos comenzado con la configuración inicial

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//Entender JSON
+app.use(express.json());
+
+//Ruta principal
+app.get('/', (req, res) => {
+    res.send('Servidor de League of Legends está funcionando');
+});
+
+//Arrancamos el Servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`); 
+});
