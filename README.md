bab32ec5d8b8bd02c7ba483f1c29c07c66436658
Pedro Enrique Traverso García <63427425+petravgar@users.noreply.github.com>
2026-05-11 17:57:41 +0200

    Commit 2: Definición de datos de la API y actualización de README
    Hemos definido los datos de la API, y actualizado el README con el trabajo hecho hasta ahora y lo próximo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Lenguaje De Marcas Trabajo
-Trabajo final de marcas
+# Trabajo Final Lenguaje de Marcas: API de League of Legends 
+
+Este proyecto es una API REST desarrollada con **Node.js** y **Express** para la asignatura de Lenguajes de Marcas. La temática elegida es: League of Legends.
+
+## Descripción
+La API permite gestionar una colección de **Campeones** (recurso principal) y sus **Skins** (recurso secundario). 
+
+### Recursos
+* **Campeones:** Contiene información detallada como rol, dificultad, tipo de daño, línea, región, etc.
+* **Skins:** Colección relacionada con los campeones mediante el campo `campeonId`.
+
+## Endpoints (En construcción)
+| Método | Ruta | Descripción | Ejemplo de uso |
+| :--- | :--- | :--- | :--- |
+| GET | `/` | Comprobación del servidor | `http://localhost:3000/` |
+| GET | `/campeones` | Obtener todos los campeones | `http://localhost:3000/campeones` |
+
+---
+**Desarrollado por:** Pedro Enrique Traverso García
+**Curso:** 1° DAM
--- index.js
+++ index.js
@@ -14,3 +14,82 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`); 
 });
+
+//Recurso principal: Campeones del LoL
+let campeones = [
+    {
+        id: 1,
+        nombre: "Viego",
+        rol: "Asesino",
+        dificultad: 3,
+        daño: "Físico",
+        posicion: "Jungla",
+        esMelee: true,
+        region: "Islas de las sombras"
+
+    },
+
+    {
+        id: 2, 
+        nombre: "Garen", 
+        rol: "Luchador", 
+        dificultad: 1, 
+        daño: "Físico", 
+        posicion: "Top", 
+        esMelee: true, 
+        region: "Demacia"
+    },
+
+    { 
+        id: 3, 
+        nombre: "Aphelios", 
+        rol: "Tirador", 
+        dificultad: 3, 
+        daño: "Físico", 
+        linea: "Bot", 
+        esMelee: false, 
+        region: "Targon" 
+    },
+
+    { 
+        id: 4, 
+        nombre: "Sylas", 
+        rol: "Luchador", 
+        dificultad: 3, 
+        daño: "Mágico", 
+        linea: "Mid", 
+        esMelee: true, 
+        region: "Demacia, Freljord" 
+    }
+
+
+];
+
+//Recurso secundario: skins del LoL
+
+let skins = [
+    {
+        id: 1,
+        campeonId: 1,
+        nombre: "Viego Rey Renacido",
+        precio: 250
+    },
+    {
+        id: 2,
+        campeonId: 2,
+        nombre: "Garen Academia de Combate",
+        precio: 10
+    },
+    {
+        id: 3,
+        campeonId: 3,
+        nombre: "Aphelios Bestia Lunar",
+        precio: 10
+    },
+    {
+        id: 4,
+        campeonId: 4,
+        nombre: "Sylas Estrella Oscura",
+        precio: 20
+    }
+];
